303b8cceafb693aed547f2224b07920aa3348d01
latimko <latimko@ncsu.edu>
2016-09-06 18:09:05 -0400

    Finished employee

--- code/3/employee.py
+++ code/3/employee.py
@@ -6,9 +6,18 @@ class Employee:
 		self.name = name
 		self.age = age
 	
-	def __repr__():
-		print('Employee Name: %s, Age: %d', name, age)
+	def __repr__(self):
+		return 'Employee Name: %s, Age: %d' % (self.name, self.age)
 	
 	def __lt__(self, other):
 		return self.age < other.age
 	
+x = Employee('John', 40)
+print(x)
+y = Employee('Jack', 30)
+print(y)
+
+list = [x, y]
+print list
+list.sort()
+print list
