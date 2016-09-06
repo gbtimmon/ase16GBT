4bc5b0d8057a05c0a533900b542bd584ff362a54
latimko <latimko@ncsu.edu>
2016-09-06 17:49:18 -0400

    Employee class

---
+++ code/3/employee.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys
+import pprint
+
+class Employee:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+	
+	def __repr__():
+		pprint.pprint(self)
+	
+	def __lt__():
+		
+	
