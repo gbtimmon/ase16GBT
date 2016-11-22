d14bdd144ebb0d37ca19925c01b88c269112c80a
pwang13 <pwang13@ncsu.edu>
2016-11-21 23:56:10 -0500

    update score and energy function with fewer arguments

--- code/7/DTLZ7.py
+++ code/7/DTLZ7.py
@@ -12,39 +12,44 @@ class DTLZ7(Model):
         self.objectiveSpace = objectiveSpace
         for i in range(self.decisionSpace):
             self.decisions.append(Decisions(0, 1))
+        self.any()
 
-    def score(self, candidates):
+    def copy(self, other):
+        self.decisions = other.decisions[:]
+        self.candidates = other.candidates[:]
+        self.decisionSpace = other.decisionSpace
+        self.objectiveSpace = other.objectiveSpace
+
+    def score(self):
         # use sum of objectives as score
-        return fabs(sum(self.objs(candidates)))
+        return fabs(sum(self.fi()))
 
-    def fm(self, candidates, objectives):
-        g = 1 + 9 / (self.decisionSpace - self.objectiveSpace + 1) * sum(candidates[self.objectiveSpace - 1:])
+    def fm(self, objectives):
+        g = 1 + 9 / (self.decisionSpace - self.objectiveSpace + 1) * sum(self.candidates[self.objectiveSpace - 1:])
         h = self.objectiveSpace
         for x in range(self.objectiveSpace - 1):
             h += (objectives[x] / (1 + g)) * (1 + sin(3 * pi * objectives[x]))
 
         objectives.append((1 + g) * h)
 
-    def fi(self, candidates):
+    def fi(self):
         objectives = []
 
         # for fis before the last one
         for i in xrange(self.objectiveSpace - 1):
-            objectives.append(candidates[i])
+            objectives.append(self.candidates[i])
 
         # calculate and append the last f
-        self.fm(candidates, objectives)
+        self.fm(objectives)
 
         # return
         return objectives
 
-    def objs(self, candidates):
-        return self.fi(candidates)
 
 
 if __name__ == "__main__":
     DTLZ7 = DTLZ7()
     DTLZ7.any()
     print(DTLZ7.candidates)
-    print(DTLZ7.fi(DTLZ7.candidates))
-    print(DTLZ7.score(DTLZ7.candidates))
+    print(DTLZ7.fi())
+    print(DTLZ7.score())
