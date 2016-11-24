4519e92bdabd7d02edc7961513550a4c17c710cd
pwang13 <pwang13@ncsu.edu>
2016-11-24 10:20:05 -0500

    beta version 1 finished

---
+++ code/7/main.py
@@ -0,0 +1,23 @@
+from DTLZ7 import DTLZ7
+from DE import de
+from SA import sa
+from MWS import mws
+from stats import rdivDemo
+
+from helper.sk import *
+if __name__ == '__main__':
+    repeats = 20
+    # repeats = 1
+    for model in [DTLZ7]:
+        data = []
+        baseline = [model() for _ in xrange(repeats)]
+        for i,optimizer in enumerate([sa,mws,de]):
+        # for i,optimizer in enumerate([de]):
+            opt_rpt = []
+            opt_rpt.append(optimizer.func_name)
+            for j in range(repeats):
+                print "\nStarting Model = %s Optimizer = %s Repeat No = %s \n" % ("DTLZ7",str(optimizer.__name__),str(j+1))
+                res = optimizer(model, baseline[j])
+                opt_rpt.append(res.score())
+            data.append(opt_rpt)
+        rdivDemo(data)
